46da1810d388fcecf2cc21aa13c600c3b0a972d0
abdurehman-kero <keroabdurehman@gmail.com>
2026-02-05 14:29:10 -0500

    header added

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -2,16 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='App'>
-        Hello
+    <div className='App'>
 
-      </div>
+      <Home/>
+    </div>
     </>
   )
 }
