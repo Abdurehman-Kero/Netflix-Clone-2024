f3fc9999a6f1d82c919fbd1dbaf4cd8081b16da5
abdurehman-kero <keroabdurehman@gmail.com>
2026-02-14 23:44:26 -0500

    banner styling added

--- netflix-clone/src/Components/Banner/Banner.jsx
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,33 +1,45 @@
 import "./banner.css";
-import Axios from "../../utils/base";
+import Axios from "../../utils/axios.js";
 import { useEffect, useState } from "react";
 import fetchData from "../../utils/request";
 
+const truncate = (string, n) => {
+  return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+};
+
 const Banner = () => {
+  const [movie, setMovie] = useState({});
 
-const [movie, setMovie] = useState({});
+ useEffect(() => {
+   const fetchMovie = async () => {
+     try {
+       const response = await Axios.get(fetchData.getUpcomingMovies);
 
-useEffect(() => {
-  (async () => {
-    try {
-      const request = await Axios.get(fetchData.getUpcomingMovies);
-      setMovie(
-        request.data.results[
-          Math.floor(Math.random() * request.data.results.length)
-        ],
-      );
-    } catch (error) {
-      console.log("error", error);
-    }
-  })();
-}, []);
+       const results = response.data?.results;
+
+       if (!results || results.length === 0) {
+         console.warn("No movies returned");
+         return;
+       }
+
+       setMovie(results[Math.floor(Math.random() * results.length)]);
+     } catch (error) {
+       console.error("Fetch error:", error);
+     }
+   };
+
+   fetchMovie();
+ }, []);
+
 
   return (
     <div
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundImage: movie?.backdrop_path
+          ? `url("https://image.tmdb.org/t/p/original${movie.backdrop_path}")`
+          : "none",
         backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
       }}
@@ -36,14 +48,17 @@ useEffect(() => {
         <h1 className="banner__title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
+
         <div className="banner__buttons">
           <button className="banner__button play">Play</button>
           <button className="banner__button">My List</button>
         </div>
+
         <h1 className="banner__description">
           {truncate(movie?.overview, 150)}
         </h1>
       </div>
+
       <div className="banner__fadeBottom" />
     </div>
   );
